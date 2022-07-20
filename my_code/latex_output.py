db9ccdc2fb3587a5d92832e9efe0bd4e6e5668e8
semmed <semmed@ccom.unh.edu>
2022-07-20 22:49:20 +0000

    Backup

---
+++ my_code/latex_output.py
@@ -0,0 +1,59 @@
+# Some convenience function for creation of Latex expressions
+#
+# Semme J. Dijkstra 4/8/2022
+
+
+from IPython.display import display, Math
+from IPython.display import Latex as lt
+from IPython.display import display, Math
+import numpy as np
+
+def display_matrix(matrix):
+    data = ''
+    for line in matrix:
+        if not isinstance(line, np.ndarray):
+            data += ' %.4f' % line + r' \\'
+            continue
+        if len( line) == 1:
+            data += ' %.4f' % line + r' \\'
+            continue
+        for element in line[:-1]:
+            data += ' %.4f&' % element
+        data += ' %.4f' % line[-1]
+        data += r'\\' + '\n'
+    display(Math('\\begin{bmatrix} \n%s\end{bmatrix}' % data))
+    
+def latex_matrix(matrix):
+    data = ''
+    for line in matrix:
+        if not isinstance(line, np.ndarray):
+            data += ' %.4f' % line + r' \\'
+            continue
+        if len( line) == 1:
+            data += ' %.4f' % line + r' \\'
+            continue
+        for element in line[:-1]:
+            data += ' %.4f&' % element
+        data += ' %.4f' % line[-1]
+        data += r'\\' + '\n'
+    print('\\begin{bmatrix} \n%s\end{bmatrix}' % data)
+    
+def display_tensor(tensor):
+    if tensor.ndim <= 2:
+        display_matrix(tensor)
+    data = ''
+    
+#     for line in matrix:
+#         if not isinstance(line, np.ndarray):
+#             data += ' %.4f' % line + r' \\'
+#             continue
+            
+#         if len( line) == 1:
+#             data += ' %.4f' % line + r' \\'
+#             continue
+            
+#         for element in line[:-1]:
+#             data += ' %.4f&' % element
+#         data += ' %.4f' % line[-1]
+#         data += r'\\' + '\n'
+#     display(Math('\\begin{bmatrix} \n%s\end{bmatrix}' % data))
---
+++ my_code/plottransform.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plottransform(B,Bt):
+    # Don't change this, just checking whether the arguments are usable
+    if not isinstance(B, (np.ndarray)):
+        print('B must be a numpy array')
+        return
+    if not isinstance(Bt, (np.ndarray)):
+        print('Bt must be a numpy array')
+        return
+
+    if not (B.shape[0] == 2) or B.shape[1] < 2 or not len(B.shape)==2:
+        print('Matrix shape '+str(B.shape)+' does not match expected polygon shape (2 x >=2)')
+        return
+    if not (Bt.shape[0] == 2) or Bt.shape[1] < 2 or not len(Bt.shape)==2:
+        print('Matrix shape '+str(Bt.shape)+' does not match expected polygon shape (2 x >=2)')
+        return
+    
+    # Add your code after this line
+
+    plt.axis('equal')
+    plt.fill(B[0,:],B[1,:],'C0', ec='k',zorder=0)
+    for i, x in enumerate(B[0,:]):
+        plt.plot([B[0,i], Bt[0,i]],[B[1,i],Bt[1,i]],'k--',zorder=1)
+
+    plt.fill(Bt[0,:],Bt[1,:],'C1', ec='k', alpha=.8,zorder=2)
+
+    return 
+    
