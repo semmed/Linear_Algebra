e6fdbb709b6d0126a3f1cba7e82d1957ed443337
semmed <semmed@ccom.unh.edu>
2022-08-08 18:48:23 +0000

    After completion of chapter 3 and partial completion of 4

---
+++ my_code/boat.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def boat():
+    return np.asarray([[-6, 4, 6, 4, -6],[-2, -2, 0, 2, 2]])
+#      return np.asarray([[1],[1]])
--- my_code/plottransform.py
+++ my_code/plottransform.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plottransform(B,Bt):
+def plottransform(B,Bt,plot_title = ''):
     # Don't change this, just checking whether the arguments are usable
     if not isinstance(B, (np.ndarray)):
         print('B must be a numpy array')
@@ -20,8 +20,9 @@ def plottransform(B,Bt):
     # Add your code after this line
 
     plt.axis('equal')
+    plt.title(plot_title)
     plt.fill(B[0,:],B[1,:],'C0', ec='k',zorder=0)
-    for i, x in enumerate(B[0,:]):
+    for i in range(B.shape[1]):
         plt.plot([B[0,i], Bt[0,i]],[B[1,i],Bt[1,i]],'k--',zorder=1)
 
     plt.fill(Bt[0,:],Bt[1,:],'C1', ec='k', alpha=.8,zorder=2)
