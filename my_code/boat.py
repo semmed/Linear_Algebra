004e4988e8a3cc175c70d6aa9b8ffb62d240aa49
semmed <semmed@ccom.unh.edu>
2022-08-14 19:10:56 +0000

    Ch 1 Cleaned up

--- my_code/boat.py
+++ my_code/boat.py
@@ -1,5 +1,4 @@
 import numpy as np
 
 def boat():
-    return np.asarray([[-6, 4, 6, 4, -6],[-2, -2, 0, 2, 2]])
-#      return np.asarray([[1],[1]])
+    return np.asarray([[-2, -2, 0, 2, 2],[-6, 4, 6, 4, -6]])
