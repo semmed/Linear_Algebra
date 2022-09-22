81c4c3429b9e503e77c3845d829ee2108dff14ed
semmed <semmed@ccom.unh.edu>
2022-09-22 22:11:28 +0000

    Updated logos to ePOM logos

--- my_code/boat.py
+++ my_code/boat.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def boat():
-    return np.asarray([[-2, -2, 0, 2, 2],[-6, 4, 6, 4, -6]])
+    return np.asarray([[-6., 4., 6., 4., -6.],[-2., -2., 0., 2., 2.]])
--- my_code/plottransform.py
+++ my_code/plottransform.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plottransform(B,Bt,plot_title = ''):
+# 1. Add the `plot_title = ''` argument to the function declaration of `plottransform()`
+def plottransform(B, Bt, plot_title = ''):
     # Don't change this, just checking whether the arguments are usable
     if not isinstance(B, (np.ndarray)):
         print('B must be a numpy array')
@@ -17,14 +18,20 @@ def plottransform(B,Bt,plot_title = ''):
         print('Matrix shape '+str(Bt.shape)+' does not match expected polygon shape (2 x >=2)')
         return
     
-    # Add your code after this line
-
+    # Make sure that the plot is isometric along the axes
     plt.axis('equal')
+    
+    # 2. Update the code so that it will plot the title using `plt.title()`
     plt.title(plot_title)
+    
+    # Plot the untransformed shape
     plt.fill(B[0,:],B[1,:],'C0', ec='k',zorder=0)
+    
+    # Plot the lines connecting the non-transformed and transformed vertices
     for i in range(B.shape[1]):
         plt.plot([B[0,i], Bt[0,i]],[B[1,i],Bt[1,i]],'k--',zorder=1)
 
+    # Plot the transformed shape with transparency so that the original shape and/or lines can be seen if they are covered
     plt.fill(Bt[0,:],Bt[1,:],'C1', ec='k', alpha=.8,zorder=2)
 
     return 
